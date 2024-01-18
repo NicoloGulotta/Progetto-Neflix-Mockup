585d33d2ed22bf1db229623915b7c4e870e606a7
Nicolò Gulotta <Gulottanico1998@gmail.com>
2024-01-18 18:52:59 +0100

    modificata card hover

--- index.js
+++ index.js
@@ -14,4 +14,14 @@ items.forEach((el) => {
     }
 })
 
+const movieBlocks = document.getElementsByClassName("movie-block");
 
+for(movieBlock of movieBlocks) {
+    movieBlock.addEventListener("mouseenter", (event) => {
+        event.target.getElementsByTagName("div")[0].classList.add("show");
+    });
+    movieBlock.addEventListener("mouseleave", (event) => {
+        event.target.getElementsByTagName("div")[0].classList.remove("show");
+    });
+}
+console.log(movieBlocks);
